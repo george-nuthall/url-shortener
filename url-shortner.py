7b0d58111d56b7e264f658aebcf8bfb356275888
george-nuthall <77570233+george-nuthall@users.noreply.github.com>
2021-02-28 17:35:09 +0000

    Use Bitly API to shorten long urls

--- url-shortner.py
+++ url-shortner.py
@@ -1,7 +1,11 @@
 import bitly_api
 
-BITLY_GENERIC_ACCESS_TOKEN ="dfeb0739f7c506dced3a4dc35baf94f446e82d6a"
-bitly = bitly_api.Connection(access_token = BITLY_GENERIC_ACCESS_TOKEN)
-full_link = input()
-short_url = bitly.shorten(full_link) 
-print("Short URL = %s" % short_url['url'])
+def main():
+    BITLY_GENERIC_ACCESS_TOKEN = "dfeb0739f7c506dced3a4dc35baf94f446e82d6a"
+    bitly = bitly_api.Connection(access_token=BITLY_GENERIC_ACCESS_TOKEN)
+    full_link = input()
+    short_url = bitly.shorten(full_link)
+    print("Short URL = %s" % short_url['url'])
+
+if __name__ == '__main__':
+    main()
